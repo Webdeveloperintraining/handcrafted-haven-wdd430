337eb61521d0ee6556286601ed1ad45868798ab3
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2024-11-27 17:23:54 -0400

    Update nav.tsx

--- my-app/src/app/nav.tsx
+++ my-app/src/app/nav.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image';
-
+import Link from 'next/link';
 export default function Nav() {
   return (
     <nav className="nav">
       <Image src="https://i.imghippo.com/files/dljA7368qkk.png" alt="Logo" width={50} height={50} />
       <ul>
         <li>
-          <a href="/">Home</a>
+          <Link href="/">
+         Home
+          </Link>
         </li>
         <li>
-          <a href="/about">About</a>
+          <Link href="/">
+           About
+          </Link>
         </li>
       </ul>
     </nav>
