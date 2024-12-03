270f7aaaf12e787b8c3ed34745083912276377d2
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2024-12-02 22:17:01 -0400

    add nav t pages

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -1,6 +1,9 @@
+import Nav from "../nav"
+
 export default function AboutPage(): JSX.Element {
   return (
     <div className="page">
+      <Nav></Nav>
       <h1>About Us</h1>
       <p>
         Handcrafted Haven is a team of skilled craftsmen dedicated to creating unique, handcrafted items for the home.
--- my-app/src/app/profile/page.tsx
+++ my-app/src/app/profile/page.tsx
@@ -1,6 +1,10 @@
+import Nav from "../nav";
+
 export default function ProfilePage() {
   return (
+    
     <div className="page">
+      <Nav></Nav>
       <h1>Profile</h1>
       <p>This is the profile page.</p>
     </div>
